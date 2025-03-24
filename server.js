37a7c22fabf767687db6806b928323ea11b83c94
Mani <info@nucotedutech.com>
2025-03-24 13:28:04 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -4,9 +4,18 @@ const nodemailer = require("nodemailer");
 const cors = require("cors");
 require("dotenv").config();
 
+const express = require("express");
 const app = express();
-const port = 5000;
 
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, Railway!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 // Enable CORS for frontend communication
 app.use(cors());
 
